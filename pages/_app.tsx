1fc622acb55dc3f9f7aebb9cbc90da5efdf44cef
trentteinert <102979377+trentteinert@users.noreply.github.com>
2022-12-03 12:54:04 -0500

    git fucked?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import NavBar from '../components/navcomponents/NavBar';
+import NavBar from './../components/navcomponents/NavBar';
 import ScrollButton from '../components/ScrollButton';
 
 export default function App({ Component, pageProps }: AppProps) {
